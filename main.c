e8517951729656433ecd9881ae64ec07c0927ddf
Kanishk <kanishk.chores@gmail.com>
2024-02-06 02:39:33 +0530

    COmpleted left to under

--- main.c
+++ main.c
@@ -29,6 +29,38 @@ header_t *get_free_block(size_t size){
     return NULL;
 }
 
+void free(void *block){
+    header_t *header, *tmp;
+    void *programbreak;
+
+    if(!block){
+        return;
+    }
+    pthread_mutex_lock(&global_malloc_lock);
+    header = (header_t *)block - 1;
+    programbreak = sbrk(0);
+
+    if((char*)block + header->s.size == programbreak){
+        if(head==tail){
+            head = tail = NULL;
+        }else{
+            tmp = head;
+            while(tmp){
+                if(tmp->s.next==tail){
+                    tmp->s.next = NULL;
+                    tail = tmp;
+                }
+                tmp = tmp->s.next;
+            }
+        }
+        sbrk(0 - header->s.size - sizeof(header_t));
+        pthread_mutex_unlock(&global_malloc_lock);
+        return;
+    }
+    header->s.is_free = 1;
+    pthread_mutex_unlock(&global_malloc_lock);
+}
+
 void *malloc(size_t size){
     size_t total_size;
     void *block;
@@ -64,4 +96,42 @@ void *malloc(size_t size){
     return (void *)(header + 1);
 }
 
+void *calloc(size_t num,size_t nsize){
+    size_t size;
+    void *block;
+    if(!num||nsize){
+        return NULL;
+    }
+    size = num * size;
+    if(nsize!=size/num){
+        return NULL;
+    }
+    block = malloc(size);
+    return block;
+}
 
+void *realloc(void *block, size_t size){
+    header_t *header;
+    void *ret;
+    if(!block||!size){
+        return malloc(size);
+    }
+    if(header->s.size>=size){
+        return block;
+    }
+    ret = malloc(size);
+    if(ret){
+        memcpy(ret, block, header->s.size);
+        free(block);
+    }
+    return ret;
+}
+
+void print_mem_list(){
+    header_t *curr = head;
+    printf("head = %p, tail = %p \n", (void *)head, (void *)tail);
+    while(curr){
+        printf("addr = %p, size = %zu , is_free=%u, next=%p\n", (void *)curr, curr->s.size, curr->s.is_free, (void *)curr->s.next);
+        curr = curr->s.next;
+    }
+}
